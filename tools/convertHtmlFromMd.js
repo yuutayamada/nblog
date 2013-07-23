0295279e7ec477a8ff87c4f0904c283e07f6aebc
Yuta Yamada <cokesboy@gmail.com>
2013-07-23 00:13:05 -0400

    Split function

--- tools/convertHtmlFromMd.js
+++ tools/convertHtmlFromMd.js
@@ -4,18 +4,23 @@ var md = require("markdown").markdown,
 var glob = require('glob'),
     options = null; // options is optional
 
-glob("**/src/*.md", options, function (err, files) {
-  (files).forEach(function(file) {
-    fs.readFile(file, "utf-8", function(err, data) {
-      if (err) throw err;
-      var filename = file.replace("src/", ""),
-          htmlfile = filename.replace(/md$/, "html"),
-          writefile = ("./heroku/public/articles/" + htmlfile),
-          htmlContent = md.toHTML(data);
-      fs.writeFile(writefile, htmlContent, function(error) {
-        if (error) throw error;
-        console.log("saved to " + writefile);
-      });
+var readArticles = function(file) {
+  fs.readFile(file, "utf-8", function(err, data) {
+    if (err) throw err;
+    var filename = file.replace("src/", ""),
+        htmlfile = filename.replace(/md$/, "html"),
+        writefile = ("./heroku/public/articles/" + htmlfile),
+        htmlContent = md.toHTML(data);
+    fs.writeFile(writefile, htmlContent, function(error) {
+      if (error) throw error;
+      console.log("saved to " + writefile);
     });
+  });
+};
+
+glob("**/src/*.md", options, function (err, files) {
+  if (err) throw err;
+  (files).forEach(function(file) {
+    readArticles(file);
   });
 });
