72c46dcd4b83d0c0614159818a661e54a8d0f4d2
Yuta Yamada <cokesboy@gmail.com>
2013-07-23 00:04:37 -0400

    Delete needless the var declarations

--- tools/convertHtmlFromMd.js
+++ tools/convertHtmlFromMd.js
@@ -8,10 +8,10 @@ glob("**/src/*.md", options, function (err, files) {
   (files).forEach(function(file) {
     fs.readFile(file, "utf-8", function(err, data) {
       if (err) throw err;
-      var filename = file.replace("src/", "");
-      var htmlfile = filename.replace(/md$/, "html");
-      var writefile = ("./heroku/public/articles/" + htmlfile);
-      var htmlContent = md.toHTML(data);
+      var filename = file.replace("src/", ""),
+          htmlfile = filename.replace(/md$/, "html"),
+          writefile = ("./heroku/public/articles/" + htmlfile),
+          htmlContent = md.toHTML(data);
       fs.writeFile(writefile, htmlContent, function(error) {
         if (error) throw error;
         console.log("saved to " + writefile);
